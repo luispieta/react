784f62e3f5571091b70a682ed6714cc7cd7faa81
luispieta <lufelipepieta@gmail.com>
2025-06-09 20:41:36 -0300

    fix: Deletando arquivo desnecessário

--- src/utils/localStorageHelpers.js
+++
@@ -1,9 +0,0 @@
-export const getUsers = () => {
-  return JSON.parse(localStorage.getItem('usuarios')) || [];
-};
-
-export const saveUser = (user) => {
-  const users = getUsers();
-  users.push(user);
-  localStorage.setItem('usuarios', JSON.stringify(users));
-};
